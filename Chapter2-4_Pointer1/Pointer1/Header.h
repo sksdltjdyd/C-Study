697a64709b8ed8d77b029dffe844226aa1cbafa2
SUROM <opp92@naver.com>
2025-08-15 12:02:55 +0900

    Update

---
+++ Chapter2-4_Pointer1/Pointer1/Header.h
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+
+int GetInput();
+
+int sum(int a, int b);
+
+
+int sub(int a, int b);
+
+
+int mul(int a, int b);
+
+int Div(int a, int b);
+
+char GetOper();
