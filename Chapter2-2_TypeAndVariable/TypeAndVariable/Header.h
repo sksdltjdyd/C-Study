47285568423b6cf4e3f1d5195929dba9a59c3154
SUROM <opp92@naver.com>
2025-08-15 11:59:46 +0900

    Update

---
+++ Chapter2-2_TypeAndVariable/TypeAndVariable/Header.h
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+float inputValue();
+
+float add(float a, float b);
+
+float sub(float a, float b);
+
+float mul(float a, float b);
+
+float div(float a, float b);
+
+float mod(float a, float b);
+
+char getoper();
+
+char getWord();
